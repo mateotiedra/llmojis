6474cc7e1df919287482d05129d1d7fc16ae4615
Mateo Tiedra <mateo@tiedra.com>
2025-02-19 18:28:11 +0100

    update style format

--- src/components/DetailedButton.jsx
+++ src/components/DetailedButton.jsx
@@ -4,13 +4,15 @@ import { HashLink } from 'react-router-hash-link';
 const DetailedButton = ({ emoji, title, description, onClick, to }) => {
   return (
     <button
-      className='fly-hover border p-4 rounded-md text-center items-stretch'
+      className='fly-hover border rounded-md text-center items-stretch'
       onClick={onClick}
     >
       <HashLink to={to}>
-        <div className='text-3xl m-4'>{emoji}</div>
-        <div className='font-bold text-lg mt-2'>{title}</div>
-        <div className='text-sm text-gray-600 my-1 max-w-40'>{description}</div>
+        <div className='h-full w-full p-4'>
+          <p className='!text-3xl m-4'>{emoji}</p>
+          <p className='font-bold text-lg mt-2'>{title}</p>
+          <p className='!text-sm text-gray-600 my-1 max-w-40'>{description}</p>
+        </div>
       </HashLink>
     </button>
   );
