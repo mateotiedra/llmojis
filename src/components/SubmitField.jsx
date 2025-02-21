61fd038c67766106218536df6450335ffbd880f5
Mateo Tiedra <mateo@tiedra.com>
2025-02-21 21:20:33 +0100

    improce submit field

--- src/components/SubmitField.jsx
+++ src/components/SubmitField.jsx
@@ -1,17 +1,18 @@
 import PropTypes from 'prop-types';
 import { FaArrowUp } from 'react-icons/fa6';
 
-const SubmitField = ({ handleSubmit, onChange }) => {
+const SubmitField = ({ handleSubmit, onChange, inputValue }) => {
   return (
     <form
-      className='rounded-2xl border py-4 px-5 w-full max-w-md flex flex-row'
+      className='rounded-2xl border px-5 w-full max-w-md flex flex-row'
       onSubmit={handleSubmit}
     >
       <input
-        className='outline-none flex-grow'
+        className='outline-none flex-grow py-4'
         type='text'
         onChange={(e) => onChange(e.target.value)}
         placeholder='Guess the concept'
+        value={inputValue}
       />
       <button className='p-2 hover:cursor-pointer' type='submit'>
         <FaArrowUp />
@@ -23,6 +24,7 @@ const SubmitField = ({ handleSubmit, onChange }) => {
 SubmitField.propTypes = {
   handleSubmit: PropTypes.func,
   onChange: PropTypes.func,
+  inputValue: PropTypes.string,
 };
 
 export default SubmitField;
